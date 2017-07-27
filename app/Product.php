440495392e6c1863c04ff4e3987e54d1bef9a9a1
Rodrigo Argumedo <rodrigo.argumedo@icloud.com>
2017-07-26 23:22:59 -0500

    Accept only the following parameters in Products

--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    // Whitelist the ones that are acceptable from the model
+    protected $fillable = ['name', 'description', 'price'];
 }
