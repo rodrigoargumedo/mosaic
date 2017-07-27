b4aea0375f5db9ee33a4a2023000688fd7c45caa
Rodrigo Argumedo <rodrigo.argumedo@icloud.com>
2017-07-26 23:48:10 -0500

    Update the new view

--- resources/views/product/new.blade.php
+++ resources/views/product/new.blade.php
@@ -11,8 +11,20 @@
       <hr>
       
       <form method="POST" action="/product">
-        <div class="form-group">
-          
+        {{csrf_field()}}
+        <div class="row">
+          <div class="col col-sm-3">
+            <input class="form-control" name="name" placeholder="Enter product name" required/>
+          </div>
+          <div class="col col-sm-6">
+            <textarea class="form-control" name="description" placeholder="Enter description" rows="3"></textarea>
+          </div>
+          <div class="col col-sm-3">
+            <input class="form-control" name="price" placeholder="Enter price (in $USD)"/>
+          </div>
+          <div class="form-group">
+            <button class="btn btn-primary" type="submit" style="background-color:#F15A2D;color:#fff;border-color:#F15A2D"/>Submit</button>
+          </div>
         </div>
       </form>
       
